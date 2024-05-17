976ef2241fbed260a8db39c2bf6176a679bbfe44
rong-xyz <rong@pathintegral.xyz>
2024-05-17 10:17:55 +0800

    'name changes'

--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 import subprocess
-import os
 from datetime import datetime
 
 alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
